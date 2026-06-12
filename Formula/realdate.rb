75fcaa0e1c88076143d5af03b0c6c0ed90a7dd45
Anton Fillmann <anton.fillmann@icloud.com>
2026-06-12 16:50:52 +0200

    updated realdata to v1.1.0 - Improved Timestamp Precision.

--- Formula/realdate.rb
+++ Formula/realdate.rb
@@ -1,8 +1,8 @@
 class Realdate < Formula
   desc "A Swift CLI tool that extracts dates from filenames, sets macOS file timestamps, and cleans up filenames for better file system sorting."
   homepage "https://github.com/AntonFill/RealDate"
-  url "https://github.com/AntonFill/RealDate/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "7c4f0101f37e7053b6918803496abd249a923a6141a653e89e153b8752615628"
+  url "https://github.com/AntonFill/RealDate/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "08f1391e653a8af6b8c25bd5f6355b58abed96dd782b50c22355edaccb032f0b"
   license "MIT"
 
   depends_on :xcode => ["13.0", :build]
