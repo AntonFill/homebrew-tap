74a3725f2989479cbff1b51d6ec7c13ad6b3b4d8
Anton Fillmann <anton.fillmann@icloud.com>
2026-08-17 19:43:13 +0200

    realdate 1.3.0

--- Formula/realdate.rb
+++ Formula/realdate.rb
@@ -1,8 +1,8 @@
 class Realdate < Formula
   desc "Set macOS file timestamps from the date in the filename"
-  homepage "https://github.com/AntonFill/RealDate"
-  url "https://github.com/AntonFill/RealDate/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "24f5c6eea99c4b6efedc74ced402cb4157dd95d0bf331bc013d34cb023afbfc3"
+  homepage "https://github.com/AntonFill/realdate"
+  url "https://github.com/AntonFill/realdate/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "25af87baa6fa0bb97530275212247f676255916f37cd5e5339cac7430fdbce94"
   license "MIT"
 
   depends_on xcode: ["16.0", :build]
